4dd944edcb6489dd4286b8bc2550d1ae05fe7388
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-10-13 19:04:11 +0200

    added proptypes

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,5 +1,5 @@
-import { WrapStat, TextStat } from './StatisticsStyled'
-
+import PropTypes from 'prop-types';
+import { WrapStat, TextStat } from './StatisticsStyled';
 
 export const Statistics = ({
   good,
@@ -8,13 +8,21 @@ export const Statistics = ({
   total,
   positivePercentage,
 }) => {
-    return (
-      <WrapStat>
-        <TextStat>Good: {good}</TextStat>
-        <TextStat>Neutral: {neutral}</TextStat>
-        <TextStat>Bad: {bad}</TextStat>
-        <TextStat>Total: {total}</TextStat>
-        <TextStat>Positive feedback: {positivePercentage}%</TextStat>
-      </WrapStat>
-    );
+  return (
+    <WrapStat>
+      <TextStat>Good: {good}</TextStat>
+      <TextStat>Neutral: {neutral}</TextStat>
+      <TextStat>Bad: {bad}</TextStat>
+      <TextStat>Total: {total}</TextStat>
+      <TextStat>Positive feedback: {positivePercentage}%</TextStat>
+    </WrapStat>
+  );
+};
+
+Statistics.prototype = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 }
